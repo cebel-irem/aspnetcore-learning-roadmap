4c9fda5bd55fcecfe6e9b1437f515f6f9bceedfc
Irem Nur Cebel <iremnurc08@gmail.com>
2026-09-14 00:16:07 +0300

    feat(02-dotnet): add ConfigPlayground console app and update .gitignore

---
+++ 02-dotnet/ConfigPlayground/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration; // Konfiguration
+
+//ConfigurationBuilder ile dosyayı tanıyoruz
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+    
+// Veriyi okuma
+string appName = config["AppSettings:AppName"]; //appsettings.json dosyasındaki AppSettings altındaki AppName değerini okuyoruz
+int maxRetry = int.Parse(config["AppSettings:MaxRetryCount"] ?? "3"); //appsettings.json dosyasındaki AppSettings altındaki MaxRetryCount değerini okuyoruz
+
+// Veriyi yazdırma
+Console.WriteLine($"App Name: {appName}");
+Console.WriteLine($"Max Retry Count: {maxRetry}");
